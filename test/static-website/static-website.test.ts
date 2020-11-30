7c0e5f1627856cf2d9305a2b7a03c74952432349
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2020-11-30 15:14:20 +0100

    feat(slack-events): use a HTTP API
    Use a HTTP API instead of a Rest API.
    
    HTTP APIs are optimized for building APIs that proxy to AWS Lambda
    functions and we don't need features of the Rest APIs here.

--- test/static-website/static-website.test.ts
+++ test/static-website/static-website.test.ts
@@ -1,5 +1,6 @@
 import * as assert from '@aws-cdk/assert';
-import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as apigatewayv2 from '@aws-cdk/aws-apigatewayv2';
+import * as integrations from '@aws-cdk/aws-apigatewayv2-integrations';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as route53 from '@aws-cdk/aws-route53';
 import * as cdk from '@aws-cdk/core';
@@ -12,11 +13,13 @@ beforeEach(() => {
 
 test('StaticWebsite', () => {
   const hostedZone = new route53.HostedZone(stack, 'HostedZone', { zoneName: 'my-site.com' });
-  const api = new apigateway.LambdaRestApi(stack, 'Api', {
-    handler: new lambda.Function(stack, 'Fn', {
-      code: lambda.Code.fromInline('inline'),
-      handler: 'index.handler',
-      runtime: lambda.Runtime.NODEJS_12_X,
+  const api = new apigatewayv2.HttpApi(stack, 'Api', {
+    defaultIntegration: new integrations.LambdaProxyIntegration({
+      handler: new lambda.Function(stack, 'Fn', {
+        code: lambda.Code.fromInline('inline'),
+        handler: 'index.handler',
+        runtime: lambda.Runtime.NODEJS_12_X,
+      }),
     }),
   });
 
