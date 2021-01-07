60b9fa44d1390fd6cd560b86c3dd03d1e4ffed8b
Jonathan Goldwasser <jogold@users.noreply.github.com>
2021-01-07 16:04:16 +0100

    feat(saml-identity-provider): SAML federated principal (#110)
    Add a `SamlFederatedPrincipal` to create roles assumed by the SAML identity
    provider.

--- test/saml-identity-provider/saml-identity-provider.test.ts
+++ test/saml-identity-provider/saml-identity-provider.test.ts
@@ -1,15 +1,20 @@
 import * as assert from '@aws-cdk/assert';
+import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
-import { SamlIdentityProvider } from '../../src';
+import { SamlFederatedPrincipal, SamlIdentityProvider } from '../../src';
 
 let stack: cdk.Stack;
 beforeEach(() => {
   stack = new cdk.Stack();
 });
 
-test('EmailReceiver', () => {
-  new SamlIdentityProvider(stack, 'IdentityProvider', {
+test('SamlIdentityProvider', () => {
+  const identityProvider = new SamlIdentityProvider(stack, 'IdentityProvider', {
     metadataDocument: '<?xml version="1.0" encoding="utf-8"?><EntityDescriptor ID="ID" entityID="ID"</EntityDescriptor>',
+  });
+
+  new iam.Role(stack, 'Role', {
+    assumedBy: new SamlFederatedPrincipal(identityProvider),
   });
 
   expect(assert.SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
