1a4be51f8710bd6d23af6d32550b0b65804fb49e
Jonathan Goldwasser <jogold@users.noreply.github.com>
2021-01-15 20:04:37 +0100

    fix(static-website): remove content security policy
    It's hard to find a good default for this header.

--- src/static-website/security-headers.ts
+++ src/static-website/security-headers.ts
@@ -7,5 +7,4 @@ export const SECURITY_HEADERS = {
   'X-Frame-Options': 'DENY',
   'X-XSS-Protection': '1; mode=block',
   'Referrer-Policy': 'same-origin',
-  'Content-Security-Policy': "default-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'none';",
 };
