1a54bf429e6984bf75a25486b811b8baecd5e986
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2021-12-22 14:17:36 +0100

    chore(slack-app): add integration test

---
+++ test/slack-app/slack-app.integ.ts
@@ -0,0 +1,28 @@
+import { App, CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import { Construct } from 'constructs';
+import { SlackApp } from '../../src';
+
+class TestStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const app = new SlackApp(this, 'MyApp', {
+      configurationTokenSecret: secretsmanager.Secret.fromSecretNameV2(this, 'Secret', 'slack-app-config-token'),
+      manifest: Stack.of(this).toJsonString({
+        display_information: {
+          name: 'My App',
+          description: 'A very cool Slack App deployed with CDK',
+        },
+      }),
+    });
+
+    new CfnOutput(this, 'AppId', {
+      value: app.appId,
+    });
+  }
+}
+
+const app = new App();
+new TestStack(app, 'slack-app-integ');
+app.synth();
