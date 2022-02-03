0ff1268cdbbbc57996c6b8bd83b0230996fd6df5
Jonathan Goldwasser <jogold@users.noreply.github.com>
2022-02-03 15:34:39 +0100

    feat: add exports to package.json (#155)
    Add `exports` to `package.json`.
    
    Allows to import like this:
    
    ```ts
    import { ToolkitCleaner } from 'cloudstructs/toolkit-cleaner';
    ```

--- .projenrc.js
+++ .projenrc.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { awscdk } = require('projen');
 
 const project = new awscdk.AwsCdkConstructLibrary({
@@ -25,5 +26,19 @@ const project = new awscdk.AwsCdkConstructLibrary({
   ],
   defaultReleaseBranch: 'master',
 });
+
+// Add "exports"
+const packageExports = {
+  '.': './lib/index.js',
+  './package.json': './package.json',
+  './.jsii': './.jsii',
+};
+for (const dirent of fs.readdirSync('./src', { withFileTypes: true })) {
+  if (dirent.isDirectory()) {
+    const construct = dirent.name;
+    packageExports[`./${construct}`] = `./lib/${construct}/index.js`;
+  }
+}
+project.tryFindObjectFile('package.json').addOverride('exports', packageExports);
 
 project.synth();
