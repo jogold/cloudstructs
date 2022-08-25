939ec1cbaa66c03e69854006fc4fb448bc08b534
Jonathan Goldwasser <jogold@users.noreply.github.com>
2022-08-25 14:44:57 +0000

    remove alarm topic from integ

--- test/ssl-server-test/ssl-server-test.integ.ts
+++ test/ssl-server-test/ssl-server-test.integ.ts
@@ -1,5 +1,4 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { Topic } from 'aws-cdk-lib/aws-sns';
 import { Construct } from 'constructs';
 import { SslServerTest } from '../../src';
 
@@ -7,11 +6,8 @@ class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const alarmTopic = new Topic(this, 'Topic');
-
     new SslServerTest(this, 'SslServerTest', {
       host: 'cdk.dev',
-      alarmTopic,
     });
   }
 }
