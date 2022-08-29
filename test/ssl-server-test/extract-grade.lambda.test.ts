73c6f68365f0c4a3c471e4e6d8b182dc3632da76
Jonathan Goldwasser <jogold@users.noreply.github.com>
2022-08-29 10:13:21 +0000

    handler tests

---
+++ test/ssl-server-test/extract-grade.lambda.test.ts
@@ -0,0 +1,13 @@
+import { handler } from '../../src/ssl-server-test/extract-grade.lambda';
+
+test('extracts grade', async () => {
+  const grade = await handler({
+    endpoints: [
+     { grade: 'E' },
+     { grade: 'A+' },
+     { grade: 'C' },
+    ],
+  });
+
+  expect(grade).toBe('E');
+});
