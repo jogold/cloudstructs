367e2372efd6417a196d10cc4d4fde79cc926231
Jonathan Goldwasser <jogold@users.noreply.github.com>
2022-12-31 19:13:19 +0100

    Revert "feat: use Node.js 18.x for all Lambda functions (#210)"
    This reverts commit 16d0e745ca11b8133e9e11c0f973eacfe4442df3.

--- src/toolkit-cleaner/clean-images-function.ts
+++ src/toolkit-cleaner/clean-images-function.ts
@@ -17,7 +17,7 @@ export class CleanImagesFunction extends lambda.Function {
     super(scope, id, {
       description: 'src/toolkit-cleaner/clean-images.lambda.ts',
       ...props,
-      runtime: new lambda.Runtime('nodejs18.x', lambda.RuntimeFamily.NODEJS),
+      runtime: new lambda.Runtime('nodejs16.x', lambda.RuntimeFamily.NODEJS),
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../../assets/toolkit-cleaner/clean-images.lambda')),
     });
