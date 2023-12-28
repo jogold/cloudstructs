7f662688d202e06722fca6827f2e5a945d24ae0b
Jonathan Goldwasser <jogold@users.noreply.github.com>
2023-12-28 10:55:19 +0100

    chore(deps): upgrade @slack/web-api (#249)
    Fixes #248

--- .projenrc.js
+++ .projenrc.js
@@ -74,4 +74,8 @@ for (const dirent of fs.readdirSync('./src', { withFileTypes: true })) {
 }
 project.tryFindObjectFile('package.json').addOverride('exports', packageExports);
 
+project.tryFindObjectFile('package.json').addOverride('jest.moduleNameMapper', {
+  axios: 'axios/dist/node/axios.cjs',
+});
+
 project.synth();
