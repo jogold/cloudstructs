277c25c6b8225d604d34dd5bdcdcbce9bef17862
Jonathan Goldwasser <jogold@users.noreply.github.com>
2025-07-02 09:35:28 +0200

    chore: remove Maven endpoint

--- .projenrc.js
+++ .projenrc.js
@@ -47,7 +47,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
     mavenGroupId: 'io.github.jogold',
     javaPackage: 'io.github.jogold.cloudstructs',
     mavenArtifactId: 'cloudstructs',
-    mavenEndpoint: 'https://s01.oss.sonatype.org',
   },
   lambdaOptions: {
     runtime: awscdk.LambdaRuntime.NODEJS_22_X,
