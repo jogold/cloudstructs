41c6f73289214257c3a1ce1aa4ed34098fce1c3f
Jonathan Goldwasser <jonathan@goldwasserexchange.be>
2026-01-06 23:18:42 +0100

    feat(url-shortener): use a Lambda function URL

---
+++ src/url-shortener/redirect.lambda.ts
@@ -0,0 +1,30 @@
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({});
+
+export async function handler(event: AWSLambda.LambdaFunctionURLEvent): Promise<AWSLambda.LambdaFunctionURLResult> {
+  try {
+    const key = event.rawPath.substring(1); // remove first slash
+
+    const data = await s3Client.send(new GetObjectCommand({
+      Bucket: process.env.BUCKT_NAME,
+      Key: key,
+    }));
+
+    if (!data.Body) {
+      throw new Error('No body');
+    }
+
+    const redirect = JSON.parse(await data.Body.transformToString());
+
+    return {
+      statusCode: 301,
+      headers: {
+        Location: redirect.url,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return { statusCode: 404 };
+  }
+}
